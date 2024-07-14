77dc1762d18fb778cfabb47cb3a6a53ce0243346
yalanin <yalanin@gmail.com>
2024-07-14 22:20:02 +0800

    商品功能－實作刪除商品

--- src/main/java/com/yalanin/springboot_mall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/yalanin/springboot_mall/service/impl/ProductServiceImpl.java
@@ -26,4 +26,9 @@ public class ProductServiceImpl implements ProductService {
     public void updateProduct(Integer productId, ProductRequest productRequest) {
         productDao.updateProduct(productId, productRequest);
     }
+
+    @Override
+    public void deleteProductById(Integer productId) {
+        productDao.deleteProductById(productId);
+    }
 }
