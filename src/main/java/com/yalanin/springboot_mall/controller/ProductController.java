d6ec56d206c521a6dd19bd9fa03186e6d74494e0
yalanin <yalanin@gmail.com>
2024-07-31 23:23:27 +0800

    商品功能－實作查詢商品列表（分頁）

--- src/main/java/com/yalanin/springboot_mall/controller/ProductController.java
+++ src/main/java/com/yalanin/springboot_mall/controller/ProductController.java
@@ -8,11 +8,15 @@ import com.yalanin.springboot_mall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 
+@Validated
 @RestController
 public class ProductController {
     @Autowired
@@ -25,12 +29,17 @@ public class ProductController {
             @RequestParam(required = false) String search,
             // 排序
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "DESC") String sort) {
+            @RequestParam(defaultValue = "DESC") String sort,
+            // 分頁
+            @RequestParam(defaultValue = "1") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset) {
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
         List<Product> productList = productService.getProducts(productQueryParams);
         return ResponseEntity.status(HttpStatus.OK).body(productList);
     }
--- src/main/java/com/yalanin/springboot_mall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/yalanin/springboot_mall/dao/impl/ProductDaoImpl.java
@@ -31,17 +31,23 @@ public class ProductDaoImpl implements ProductDao {
                 "FROM product WHERE 1=1";
         Map<String, Object> map = new HashMap<>();
 
+        // 查詢
         if(productQueryParams.getCategory() != null) {
             sql = sql + " AND category = :category";
             map.put("category", productQueryParams.getCategory().name());
         }
-
         if(productQueryParams.getSearch() != null) {
             sql = sql + " AND product_name LIKE :search";
             map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
 
+        // 排序
         sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
+
+        // 分頁
+        sql = sql + " LIMIT :limit OFFSET :offset";
+        map.put("limit", productQueryParams.getLimit());
+        map.put("offset", productQueryParams.getOffset());
 
         List<Product> productList = namedParameterJdbcTemplate.query(sql, map, new ProductRowMapper());
         return productList;
